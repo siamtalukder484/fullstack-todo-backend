0bfed30b54f7ce4608909cdc944adb30af3e911d
Siam Talukder <73644547+siamtalukder484@users.noreply.github.com>
2023-07-31 19:03:34 +0600

    socket added

--- index.js
+++ index.js
@@ -5,13 +5,21 @@ const dbConnection = require('./configuration/dbConnection.js')
 const routes = require("./routes")
 var cors = require('cors');
 
+const http = require('http');
+const socketIO = require('socket.io');
+
+const server = http.createServer(app);
+const io = socketIO(server);
+
 app.use(cors())
 dbConnection()
 app.use(express.json())
 app.use(routes);
 
+
+
 app.get('/', function (req, res) {
   res.send('Todo Application')
 })
 
-app.listen(8000)
+server.listen(8000)
